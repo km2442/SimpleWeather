082f3afebef213e707abedfec01efdca99928418
Kamil <urlikodd@gmail.com>
2019-08-21 22:54:55 +0200

    Added app bar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <v-app>
+    <Navbar></Navbar>
     <router-view></router-view>
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   components: {
-    
+    Navbar: () => import(/* webpackChunkName: "Index" */ './components/Navigation/Navbar')
   },
   data: () => ({
     
---
+++ src/components/Navigation/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+      <v-app-bar
+      color="teal"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Page title</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
